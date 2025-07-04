101c446dbbe80a7fffe16f1d4b037ab98d23282f
arvindkumar20 <arvindkumar8960905167@gmail.com>
2025-07-04 12:53:23 +0530

    search filter applied

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,46 +1,93 @@
-import React from "react";
-import { IoSearch } from "react-icons/io5";
+import React, { useEffect, useState } from "react";
+import { IoPlayForward, IoSearch } from "react-icons/io5";
 import { CiSearch } from "react-icons/ci";
+const history = [
+  {
+    url: "https://my.alfred.edu/zoom/_images/foster-lake.jpg",
+    author: "Arvind",
+    prompt: "nature",
+    date: "3/07/2025",
+  },
+  {
+    url: "https://my.alfred.edu/zoom/_images/foster-lake.jpg",
+    author: "kalit",
+    prompt: "nature",
+    date: "3/07/2025",
+  },
+  {
+    url: "https://my.alfred.edu/zoom/_images/foster-lake.jpg",
+    author: "asmit",
+    prompt: "nature",
+    date: "3/07/2025",
+  },
+  {
+    url: "https://my.alfred.edu/zoom/_images/foster-lake.jpg",
+    author: "subh",
+    prompt: "nature",
+    date: "3/07/2025",
+  },
+  {
+    url: "https://my.alfred.edu/zoom/_images/foster-lake.jpg",
+    author: "singh ",
+    prompt: "nature",
+    date: "3/07/2025",
+  },
+];
 
+import { MdDownloadForOffline } from "react-icons/md";
 export default function History() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [FiltredHistory, setFilteredHistory] = useState(history);
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  useEffect(() => {
+    if (searchTerm !== "" && searchTerm !== null) {
+      const applyFilter =
+        history.length > 0
+          ? history.filter(
+              (img) =>
+                img.prompt.toLowerCase().includes(searchTerm.trim().toLowerCase()) ||
+                img.author.toLowerCase().includes(searchTerm.trim().toLowerCase()) ||
+                img.date.toLowerCase().includes(searchTerm.trim().toLowerCase())
+            )
+          : history;
+      setFilteredHistory(applyFilter);
+    }
+  }, [searchTerm]);
+
   return (
     <div>
       <section className="history">
         <h1>Generated images</h1>
         <div className="search-form">
           <button>
-         <CiSearch size={23}/>
-
+            <CiSearch size={23} />
           </button>
-          <input type="text" placeholder="Search" />
+          <input type="text" placeholder="Search" onChange={handleChange} />
         </div>
       </section>
 
       <section className="image-container-prt">
-        <div className="image-container">
-          <img
-            src="https://my.alfred.edu/zoom/_images/foster-lake.jpg"
-            alt="generated here"
-          />
-        </div>
-        <div className="image-container">
-          <img
-            src="https://my.alfred.edu/zoom/_images/foster-lake.jpg"
-            alt="generated here"
-          />
-        </div>
-        <div className="image-container">
-          <img
-            src="https://my.alfred.edu/zoom/_images/foster-lake.jpg"
-            alt="generated here"
-          />
-        </div>
-        <div className="image-container">
-          <img
-            src="https://my.alfred.edu/zoom/_images/foster-lake.jpg"
-            alt="generated here"
-          />
-        </div>
+        {FiltredHistory.map((image) => {
+              return (
+                <section className="image-container card">
+                  <img src={image.url} alt="" />
+                  <span className="image-details">
+                    <h3 className="author">{image.author}</h3>
+                    <h3 className="date">{image.date}</h3>
+                  </span>
+                  <span className="image-details">
+                    <h3 className="prompt">{image.prompt}</h3>
+                    <h3 className="download">
+                      {<MdDownloadForOffline size={20} />}
+                    </h3>
+                  </span>
+                </section>
+              );
+            })}
       </section>
     </div>
   );
